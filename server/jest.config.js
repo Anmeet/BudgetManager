ed2e16f2e68a34982cf73b85467a5326080fbe21
Amit <anmeet619@gmail.com>
2024-01-09 18:12:44 +1100

    refactor(added commitizen package for meaningful commit): added commitzen packages

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,8 +4,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
+  restoreMocks: true,
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  coveragePathIgnorePatterns: ['node_modules', 'dist/config', 'dist/app.js'],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
 };
