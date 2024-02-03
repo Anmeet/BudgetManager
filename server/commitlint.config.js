e462f8d8afa50a1a15a1d8dc53773fd0e3e54383
Amit <anmeet619@gmail.com>
2024-02-03 17:34:37 +1100

    refactor(filename): remove unnecessary code from server

--- server/commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
-    'footer-max-line-length': [1, 'always', 72],
-  },
-};
